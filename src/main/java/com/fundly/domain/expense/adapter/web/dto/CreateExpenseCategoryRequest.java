ee1748a8ab311ea1c406ccac4512f6cbc9de7125
Kyriaki Darivaki <t8210030@aueb.gr>
2025-10-10 11:29:52 +0300

    add missing description char limit

--- src/main/java/com/fundly/domain/expense/adapter/web/dto/CreateExpenseCategoryRequest.java
+++ src/main/java/com/fundly/domain/expense/adapter/web/dto/CreateExpenseCategoryRequest.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class CreateExpenseCategoryRequest {
     @NotBlank(message = "Name is required.")
     private String name;
 
+    @Size(max = 255, message = "Description cannot exceed 255 characters.")
     private String description;
 
     @NotBlank(message = "Icon is required.")
